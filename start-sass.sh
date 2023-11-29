e115519c5b99083987c7d2602b48dd2edc967bfb
imnaK <mc.yama1000@gmail.com>
2023-11-29 21:50:59 +0100

    cleanup

--- start-sass.sh
+++ start-sass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 directory=$(dirname "$BASH_SOURCE")
-sass "$directory"/styles/:"$directory"/css/ --watch --no-source-map
+sass "$directory"/scss/:"$directory"/css/ --watch --no-source-map
 
