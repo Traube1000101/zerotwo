2569c64e67ac402ddaa78ecda6a0e6c1a0083624
imnaK <mc.yama1000@gmail.com>
2023-11-30 19:45:22 +0100

    added rotation animation to text

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const multiplier = 0.03;
+
 document.addEventListener("DOMContentLoaded", function(event) {
   // get element on which the hover effect should be applied
   const el = document.getElementById("effect");
@@ -5,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // add hover event listener
   window.addEventListener("mousemove", function(e) {
     // get x and y of mouse relative to viewport
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+    const mouseX = e.clientX - this.window.scrollX;
+    const mouseY = e.clientY - this.window.scrollY;
 
     // get x and y of element relative to viewport
     const elementRect = el.getBoundingClientRect();
@@ -15,5 +17,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // change style of element background to radial gradient with radius of 50px and set middle of gradient to mouse position
     el.style.backgroundImage = `radial-gradient(circle at ${mouseX - elX}px ${mouseY - elY}px, pink ${420}px, red ${420}px, red ${840}px, black ${840}px)`;
+    el.style.transform = "rotateY(" +
+    (mouseX - window.outerWidth / 2) * multiplier * -1 +
+    "deg) rotateX(" +
+    (mouseY - window.outerHeight / 2) * multiplier +
+    "deg)"
   });
 });
