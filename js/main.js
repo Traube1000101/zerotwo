cb438a6ef8aed7c89b1a0c14554eb91564c2e356
Traube1000101 <girlscoutcookies420@gmx.net>
2023-12-01 16:46:06 +0100

    optimize background; add 3d title & text container

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var ztitle = new Ztextify(".title", {
+	depth: "2.5rem",
+	layers: 24,
+	fade: false,
+	direction: "both",
+	event: "pointer",
+	eventRotation: "10deg"
+});
+
+var ztxt = new Ztextify(".text", {
+	depth: "1.5rem",
+	layers: 16,
+	fade: false,
+	direction: "both",
+	event: "none",
+	eventRotation: "0deg"
+});
